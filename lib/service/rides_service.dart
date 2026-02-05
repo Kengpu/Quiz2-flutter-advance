b2bcf7267529a3823a0f77ee5472111fe56aba6b
Keng Puthearith <puthearithkeng7@gmail.com>
2026-02-05 15:43:59 +0700

    done

--- lib/service/rides_service.dart
+++ lib/service/rides_service.dart
@@ -3,26 +3,36 @@ import '../model/ride/locations.dart';
 import '../model/ride/ride.dart';
 
 class RidesService {
-  static List<Ride> availableRides = fakeRides; // TODO for now fake data
+  static List<Ride> availableRides = fakeRides;
+  
+  static get requestedSeats => null; // TODO for now fake data
 
   //
   //  filter the rides starting from given departure location
   //
   static List<Ride> _filterByDeparture(Location departure) {
-    return [];
+    return [
+      .where((ride) => ride.departureLocation == departure).toList(),
+    ]; 
   }
 
   //
   //  filter the rides starting for the given requested seat number
   //
   static List<Ride> _filterBySeatRequested(int requestedSeat) {
-    return [];
+    return [
+       .where((ride) => ride.availableSeats >= requestedSeats).toList(),
+    ];
   }
 
   //
   //  filter the rides   with several optional criteria (flexible filter options)
   //
   static List<Ride> filterBy({Location? departure, int? seatRequested}) {
-    return [];
+    return [
+      .where((ride)
+      final matchesDeparture =
+          departure == null || ride.departureLocation == departure).toList();
+    ];
   }
 }
